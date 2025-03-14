7211200bd6bc8278bc1b18a05336ab931940dfbc
Magret <belloabiodun17@gmail.com>
2025-03-13 21:54:52 -0230

    Implement mobile responsiveness on home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,16 @@
+import "./HomePage.scss";
+
 function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <section className="home-page">
+      <section className="home-page__logo">
+        <h1 class="home-page__logo-title">QUIZ</h1>
+        <h3 class="home-page__logo-subtitle">NAME</h3>
+      </section>
+
+      <p class="home-page__text">Sentence - AI knowledge test?</p>
+      <button class="home-page__button">Start</button>
+    </section>
   );
 }
 
