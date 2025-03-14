f2e46adc9f75185e237ee587ec160487b1495f83
Maria Reyes <paolareyesrivero@gmail.com>
2025-03-13 23:05:28 -0500

    Fixed fontface import and created button component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,12 +5,14 @@ function HomePage() {
   return (
     <section className="home-page">
       <section className="home-page__logo">
-        <h1 class="home-page__logo-title">QUIZ</h1>
-        <h3 class="home-page__logo-subtitle">NAME</h3>
+        <h1 className="home-page__logo-title">QUIZ</h1>
+        <h3 className="home-page__logo-subtitle">NAME</h3>
       </section>
 
-      <p class="home-page__text">Sentence - AI knowledge test?</p>
-      <Link to="/questions"><button class="home-page__button">Start</button></Link>
+      <p className="home-page__text">Sentence - AI knowledge test?</p>
+      <Link to="/questions">
+        <button className="home-page__button">Start</button>
+      </Link>
     </section>
   );
 }
