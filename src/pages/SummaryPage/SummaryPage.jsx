7ad65f1ad489640d4ce07c90a79c1e28b4a7d389
Magret <belloabiodun17@gmail.com>
2025-03-13 23:47:42 -0230

    Implement summary page with its responsiveness

--- src/pages/SummaryPage/SummaryPage.jsx
+++ src/pages/SummaryPage/SummaryPage.jsx
@@ -1,8 +1,13 @@
+import "./SummaryPage.scss";
+
 function SummaryPage() {
   return (
-    <div>
-      <h1>Summary Page</h1>
-    </div>
+    <section className="summary">
+      <section className="summary-page">
+        <h1 className="summary-page-title">SCORE</h1>
+        <h3 className="summary-page-subtitle">1 out of 3</h3>
+      </section>
+    </section>
   );
 }
 
